02210fdab0d9bae3437dde3b7fd2389f933c2ba0
jacobjdavey <jacob.j.davey@vanderbilt.edu>
2024-01-16 21:45:43 -0600

    Created interfaces and trimming functions

---
+++ src/interfaces/legs/trimmed-flight-leg.interface.ts
@@ -0,0 +1,27 @@
+export interface TrimmedFlightLeg {
+  id: string;
+  createdTime: string;
+  Status: string;
+  Airline: string;
+  'Departure Date/Time': string;
+  'Arrival Date/Time': string;
+  'Nautical Miles': number;
+  Passengers: string[];
+  'Departure Airport': string;
+  'Arrival Airport': string;
+  'Leg ID': string;
+  'Leg Type': string;
+  'Total Miles': number;
+  'Passenger Names': string | string[];
+  'Total Cost': number;
+  'AirTable Record ID': string;
+  'Request AirTable Record ID': string[];
+  'Passenger AirTable Record IDs': string | string[];
+  'Log Airline Credit': {
+    label: string;
+    url: string;
+  };
+  'Patient Name': string[];
+  'Patient Latest Trip': string[];
+  'Is Latest Trip': string;
+}
